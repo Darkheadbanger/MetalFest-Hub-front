a7b3b0c8ce9bde86d244902eeddafcee21d563e3
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-26 22:28:21 +0700

    feat: implement CreateAccount component with registration form and styles

--- src/pages/CreateAccount.page.jsx
+++ src/pages/CreateAccount.page.jsx
@@ -1,3 +1,22 @@
-export default function CreateAccount() {
-  return null;
+import React from "react";
+import "../styles/register.css";
+
+function CreateAccount() {
+  return (
+    <aside className="register-container">
+      <form className="register-info">
+        <label htmlFor="username">Username or email</label>
+        <input type="text" name="username" id="username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">Register</button>
+        <div className="register-links">
+          <a href="#">Register</a>
+          <a href="#">Forgot login?</a>
+        </div>
+      </form>
+    </aside>
+  );
 }
+
+export default CreateAccount;
