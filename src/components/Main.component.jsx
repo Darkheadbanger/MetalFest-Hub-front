3f4a28f616630652709645ff19cc1b9b29304c31
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-26 22:48:13 +0700

    feat: add Main component with festival information layout

---
+++ src/components/Main.component.jsx
@@ -0,0 +1,31 @@
+// import Connexion from "../pages/Connexion.page";
+
+function Main() {
+  return (
+    <div className="festival-connexion">
+      <div className="festival-container">
+        <div className="festival-image">
+            <img src="" alt="Festival images" />
+        </div>
+        <div className="festival-information-container">
+            <div className="festival-information">
+                <div className="festival-title">Hellfest</div>
+                <div className="festival-visit">url festivavl</div>
+            </div>
+            <div className="festival-details">
+                <p className="date"></p>
+                <p className="place"></p>
+            </div>
+            <div className="features-bands">
+                <div>Band1</div>
+            </div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
