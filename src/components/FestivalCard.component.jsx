35dd08475eefb25ea1429bdaf710b8dbebef563e
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-27 09:04:16 +0700

    feat: integrate ModifyButton with routing in FestivalCard and implement UpdateFestival layout

--- src/components/FestivalCard.component.jsx
+++ src/components/FestivalCard.component.jsx
@@ -5,6 +5,8 @@ import FestivalDetails from "./FestivalDetails.component.jsx";
 import FeaturedBands from "./FeaturedBands.component.jsx";
 import ModifyButton from "./ModifyButton.component.jsx";
 import DeleteButton from "./DeleteButton.component.jsx"
+import { Link } from "react-router-dom";
+
 import "../styles/CardFestival.css"
 export default function FestivalCard({ festival = {} }) {
   const { image, title, url, date, place, bands } = festival;
@@ -17,7 +19,12 @@ export default function FestivalCard({ festival = {} }) {
         <FestivalDetails date={date} place={place} />
         <FeaturedBands bands={bands} />
         <div className="festival-buttons">
-          <ModifyButton />
+         {(() => {
+           const id = festival._id || festival.id || '';
+           return (
+             <Link to={`/updateFestival/${id}`}>  <ModifyButton /> </Link>
+           )
+         })()}
           <DeleteButton />
         </div>
       </div>
--- src/layouts/UpdateFestival.layout.jsx
+++ src/layouts/UpdateFestival.layout.jsx
@@ -1,3 +1,14 @@
-export default function UpdateFestival() {
-  return null;
+import ModifyFestval from "../components/ModifyFestval.component";
+import Header from "../components/Header.component";
+// import { useParams } from "react-router-dom";
+
+function UpdateFestival() {
+  return (
+    <>
+      <Header />
+      <ModifyFestval festival="" />;
+    </>
+  );
 }
+
+export default UpdateFestival;
