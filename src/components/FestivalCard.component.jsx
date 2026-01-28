7b51b5d0a0a7d654d55124f420d5551dc259fa18
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-28 17:17:12 +0100

    fix: wrap FestivalCard content in Link component for improved navigation

--- src/components/FestivalCard.component.jsx
+++ src/components/FestivalCard.component.jsx
@@ -13,22 +13,26 @@ export default function FestivalCard({ festival }) {
   const { isLoggedIn } = useContext(AuthContext);
   console.log("iccci", festival.FestivalImage)
   return (
-    <div className="festival-container">
-      <FestivalImage festival={festival} />
-      <div className="festival-information-container">
-        <FestivalInfo title={festival.festivalName}/>
-        <FestivalDetails date={festival.festivalDate} place={festival.festivalLocation} />
-        <FeaturedBands bands={festival.featureBands} />
-        <div className="festival-buttons">
-        {isLoggedIn ? (
-          <Link to={`/updateFestival/${festival._id}`}> <ModifyButton /> </Link>
-        ): null}   
-        {isLoggedIn ? (
-          <DeleteButton />
-          ) : null
-        }
+    <>
+    <Link to="/festival/:festivalId">
+      <div className="festival-container">
+        <FestivalImage festival={festival} />
+        <div className="festival-information-container">
+          <FestivalInfo title={festival.festivalName}/>
+          <FestivalDetails date={festival.festivalDate} place={festival.festivalLocation} />
+          <FeaturedBands bands={festival.featureBands} />
+          <div className="festival-buttons">
+          {isLoggedIn ? (
+            <Link to={`/updateFestival/${festival._id}`}> <ModifyButton /> </Link>
+          ): null}   
+          {isLoggedIn ? (
+            <DeleteButton />
+            ) : null
+          }
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
+    </>
   );
 }
